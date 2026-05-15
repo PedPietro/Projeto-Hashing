ec919be95860f9ab084656b58c9fba7ea3f9e31a
Pietro Tiago <166723178+PedPietro@users.noreply.github.com>
2026-05-15 15:58:21 -0300

    arquivos do Chico

---
+++ HashLinear.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class HashLinear<T> : IHashing<T>
+  where T : IComparable<T>, IRegistro<T>, new()
+{
+  public List<T> Conteudo()
+  {
+    throw new NotImplementedException();
+  }
+
+  public List<string> LocaisDosDados()
+  {
+    throw new NotImplementedException();
+  }
+
+  public bool Existe(T item, out int onde)
+  {
+    throw new NotImplementedException();
+  }
+
+  private int Hash(string chave)
+  {
+    throw new NotImplementedException();
+  }
+
+  public bool Incluiu(T item)
+  {
+    throw new NotImplementedException();
+  }
+
+  public bool Excluiu(T item)
+  {
+    throw new NotImplementedException();
+  }
+}
+
